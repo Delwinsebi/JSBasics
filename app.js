33bdc1f624ffdda17e40c30b17d40d62d37b54c7
Delwinsebi <delwinsebi@gmail.com>
2025-11-30 14:27:47 +0530

    Initial commit of app.js and index.html with basic counting and grading functionality.

---
+++ app.js
@@ -0,0 +1,18 @@
+for(let i = 0; i<=5;i++){
+    console.log("Count:",i);
+}
+
+let score = 85;
+
+if(score >=90){
+    console.log("Grade: A+");
+}
+else if(score >=85){
+    console.log("Grade: A");
+}
+else if (score >= 80){
+    console.log("Grade: B+")
+}
+else{
+    console.log("Grade: B")
+}
